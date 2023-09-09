53c293543c654af6b6f9a70e0f14b2f6fc32f643
imole <dremkay71@gmail.com>
2023-09-09 18:35:55 +0100

    some chnges

--- main.c
+++ main.c
@@ -7,6 +7,8 @@ int main(int argc, char *argv[])
 	char *command = NULL;
 	size_t max_command_size = 0;
 	ssize_t actual_command_size;
+	pid_t pid;
+	int waitstatus;
 	
 	while (1)
 	{
@@ -14,7 +16,18 @@ int main(int argc, char *argv[])
 		actual_command_size = getline(&command, &max_command_size, stdin);
 		// printf("%lu\n", bytes);
 		command[actual_command_size - 1] = '\0';
-		printf("%s\n", command);
+		// printf("%s\n", command);
+
+		char *const ar[] = {command, NULL};
+		if (strcmp(command, "exit") == 0)
+			return (0);
+
+		pid = fork();
+
+		if (pid == 0)
+			execve(ar[0], ar, NULL);					
+		waitpid(pid, &waitstatus, 0);
+
 	}
 
 	return (0);
--- shell.h
+++ shell.h
@@ -4,6 +4,9 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/wait.h>
+#include <string.h>
+
 
 void displayPrompt(void);
 
